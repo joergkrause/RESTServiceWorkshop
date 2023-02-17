1d8a3f79d0d89c34c8b560f2c9f1e99bf8a8b14f
Jörg Krause <joerg@joergkrause.de>
2023-02-17 12:20:44 +0100

    Grpc Demo

---
+++ InternalGrpcService/Services/LabelService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+
+namespace InternalGrpcService.Services
+{
+  public class LabelService : Greeter.GreeterBase
+  {
+    public override async Task<LabelResponse> GetLabels(LabelRequest request, ServerCallContext context)
+    {
+      var rs = new LabelResponse();
+      //rs.Labels.Add()
+      return rs;
+    }
+  }
+}
